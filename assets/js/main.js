754a6ee51960c1153f1b62b113f00c0cc730dac0
nguyenkyanh1101 <86906320+nguyenkyanh1101@users.noreply.github.com>
2021-07-05 10:02:54 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 // CONFIG DATA 
 const CONFIGDATA = {
-    titleHeader: 'Làm người yêu tớ nhé I Love You',
-    descriptionHeader: 'Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^',
-    buttonYes: 'Đồng Ý',
+    titleHeader: 'Cậu ơi tớ có điều muốn nói <3',
+    descriptionHeader: 'Làm người yêu tớ nhé. Nếu cậu tắt đi thì có nghĩa cậu ĐỒNG Ý rồi',
+    buttonYes: 'Tớ cũng thích cậu',
     buttonNo: 'Không! Còn lâu nhé.',
     titleModar: 'Cảm ơn em vì đồng ý',
     descriptionModar: 'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
